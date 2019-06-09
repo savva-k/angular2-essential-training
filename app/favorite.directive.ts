35620da6111a79c6e9a460fa809cb830de9f415e
Savva Kodeikin <savva.kodeikin@gmail.com>
2019-06-09 20:23:14 +0300

    Trying @HostListener

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -1,10 +1,20 @@
-import { Directive, HostBinding, Input } from '@angular/core'
+import { Directive, HostBinding, HostListener, Input } from '@angular/core'
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+  }
+  @HostListener('click') onClick() {
+    this.isFavorite = !this.isFavorite;
+  }
 
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
